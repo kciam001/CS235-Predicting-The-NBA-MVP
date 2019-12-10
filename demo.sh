89b9e73b87b933ee2025d379299f296013b207c3
peraza97 <omarperaza19@gmail.com>
2019-12-09 18:11:44 -0800

    python3 issue

--- demo.sh
+++ demo.sh
@@ -4,18 +4,18 @@
 printf "\n"
 cd knnRegression
 printf "KNN predictions:\n"
-python knnRegression.py 6 ../Data/TrainReduced.csv ../Data/FinalTestData.csv
+python3 knnRegression.py 6 ../Data/TrainReduced.csv ../Data/FinalTestData.csv
 cd ..
 #Run NN script
 printf "\n"
 cd NeuralNet
 printf "Neural Network predictions:\n"
-python nn.py --test --dataset=FinalTestData
+python3 nn.py --test --dataset=FinalTestData
 cd ..
 
 #Run linear Regression script
 printf "\n"
 cd linearRegression
 printf "Linear  Regression predictions:\n"
-python -W ignore linearRegression.py
+python3 -W ignore linearRegression.py
 cd ..
